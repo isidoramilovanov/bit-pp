094bc11f03d6b9e24ce482942eea35896483d340
BIT student <classroom@bgit.rs>
2018-02-05 16:52:59 +0100

    Switch exercises

---
+++ switch/switch_exercise2.js
@@ -0,0 +1,22 @@
+var day = 7;
+
+
+switch(day) {
+    case 1: 
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    console.log('Weekday');
+    break;
+    
+    case 6:
+    case 7:
+    console.log('Weekend');
+    break;
+
+    default : 
+    console.log('Input must be a number between 1 and 7');
+    break;
+}
+
---
+++ switch/switch_exercise4.js
@@ -0,0 +1,32 @@
+var month = 5;
+
+switch(month){
+    case 12:
+    case 1:
+    case 2:
+    console.log('winter');
+    break;
+
+    case 3:
+    case 4:
+    case 5:
+    console.log('spring');
+    break;
+
+    case 6:
+    case 7:
+    case 8:
+    console.log('summer');
+    break;
+
+    case 9:
+    case 10:
+    case 11:
+    console.log('autumn');
+    break;
+
+    default:
+    console.log('It is not valid season');
+    break;
+
+}
