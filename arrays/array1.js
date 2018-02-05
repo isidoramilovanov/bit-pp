1795c1d63fbb29f501ed990a245da62a550bc319
BIT student <classroom@bgit.rs>
2018-02-05 17:59:21 +0100

    Arrays exercises

---
+++ arrays/array1.js
@@ -0,0 +1,23 @@
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(month[0], month[5], month[9]);
+
+
+
+
+var day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(day[6]);
+
+
+
+var numbers = [2, -4, 5, -2, -11];
+console.log(numbers[1], numbers[3], numbers[4]);
+
+
+var numbers1 = [5, 15, -5, 20, 12, 18, 72, 9];
+console.log(numbers1[3], numbers1[6]);
+
+
+
+var num = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(num[2]);
+console.log(num[2][1]);
