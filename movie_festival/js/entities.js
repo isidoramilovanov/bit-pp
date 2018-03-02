b5b7841b9ce3fd6ede391a7c07c06d539c2ee8a5
BIT Student <classroom@bgit.rs>
2018-03-02 18:22:51 +0100

    Festival finished

--- movie_festival/js/entities.js
+++ movie_festival/js/entities.js
@@ -2,7 +2,12 @@ function CreateMovie(name, length, genre) {
     this.movieName = name;
     this.movieLength = length;
     this.movieGenre = genre;
+    this.movieId = (function (){
+        var number = Math.floor(89999 * Math.random() +10000);
+        return number;
+    }) ();
 }
+
 
 CreateMovie.prototype.getData = function () {
     var ge = this.movieGenre.charAt(0).toUpperCase() + this.movieGenre.charAt(this.movieGenre.length - 1).toUpperCase();
@@ -10,29 +15,52 @@ CreateMovie.prototype.getData = function () {
     return movieData;
 }
 
+
 function Program(date) {
     this.date = date,
-    this.listOfMovie = []
+    this.listOfMovie = [],
+    this.programId = (function (){
+        var number = Math.floor(89999 * Math.random() +10000);
+        return number;
+    }) ();
 };
+
 
 Program.prototype.totalNumberOfMovies = function () {
     return this.listOfMovie.length
 };
 
+
 Program.prototype.getData = function () {
     var date = this.date;
     var programLength = 0;
-    var aboutMovies = "";
+    var numberOfMovie = this.listOfMovie.length;
     if(this.listOfMovie.length == 0) {
-    aboutMovies = 'Program to be announced'
+    aboutMovies = 'Program to be announced';
     }
     this.listOfMovie.forEach(function (element) {
-        programLength += element.length
+        programLength += parseInt(element.movieLength);
     });
-    this.listOfMovie.forEach(function (element) {
-        aboutMovies += (element.getData() + '\n');
-    });
-    return date + ", " + programLength + "\n" + aboutMovies;
+    
+    return date + ", " + numberOfMovie + " movies " + ", duration: " + programLength + " min ";
 };
 
 
+Program.prototype.addMovie = function (movie) {
+    var counter = 0;
+    var totalLength = 0;
+    for (i = 0; i < this.listOfMovie.length; i++) {
+        if(movie.movieName == this.listOfMovie[i].movieName ) {
+            return "Movie already in program"; 
+        } 
+        totalLength += parseInt(this.listOfMovie[i].movieLength) ;
+        if (movie.genre == this.listOfMovie[i].genre) {
+            counter++;
+        }    
+    }
+    if (counter < 4 && totalLength < (10 * 60)) {
+        this.listOfMovie.push(movie);
+    } else {
+        return 'More then 8h of movies or more than 4 of same genre'
+    }
+};
--- movie_festival/js/festival.js
+++ movie_festival/js/festival.js
@@ -42,14 +42,25 @@ function createMovie() {
     if (validationResult == error.OK) {
         var movie = new CreateMovie(titleValue, lengthValue, genreValue);
         movieList.push(movie);
+
+        var movieIndex = movie.movieId;
         var message = error.OK;
+
+        var movieInfo = movie.getData();
+
+        var movieText = document.createTextNode(movieInfo);
         var ul = document.querySelector('#movieUl');
         var li = document.createElement('li');
-        var movieInfo = movie.getData();
-        var movieText = document.createTextNode(movieInfo);
         li.appendChild(movieText);
         ul.appendChild(li);
         document.querySelector("#errorMessage").innerHTML = '';
+
+        var movieText = document.createTextNode(movieInfo);
+        var select = document.querySelector('#movieSelect');
+        var option = document.createElement('option');
+        option.appendChild(movieText);
+        option.value = movieIndex;
+        select.appendChild(option);
     }
 
     var text = document.createTextNode(message);
@@ -72,6 +83,9 @@ function createMovie() {
 
 }
 
+//create program        
+
+
 var programList = [];
 
 document.querySelector('#programButton').addEventListener('click', createProgram);
@@ -84,16 +98,85 @@ function createProgram() {
         if (dateValue == '') {
             return 'Insert date!'
         } else {
-            var program = new Program(dateValue);
-            console.log(program);
-            programList.push(program);
-            var ul = document.querySelector('#programUl');
-            var li = document.createElement('li');
-            var programInfo = program.getData();
-            var programText = document.createTextNode(programInfo);
-            li.appendChild(programText);
-            ul.appendChild(li);
+            return "OK"
         }
     }
 
+    var validationResult = validation(dateValue);
+
+    if (validationResult == 'OK') {
+        var program = new Program(dateValue);
+        programList.push(program);
+        var programIndex = program.programId;
+
+        var programInfo = program.getData();
+
+        var programText = document.createTextNode(programInfo);
+        var ul = document.querySelector('#programUl');
+        var li = document.createElement('li');
+        li.setAttribute('data-programIndex', programIndex);
+        li.appendChild(programText);
+        ul.appendChild(li);
+
+
+        var programText = document.createTextNode(programInfo);
+        var select = document.querySelector('#programSelect');
+        var option = document.createElement('option');
+        option.appendChild(programText);
+        option.value = programIndex;
+        select.appendChild(option);
+    }
+    select.value = '-';
 }
+
+
+//Add movie to program  
+
+document.querySelector('#addMovieToProgram').addEventListener('click', addMovieToProgram);
+
+function addMovieToProgram() {
+    document.querySelector("#errorText").innerHTML = '';
+    var movieSelect = document.querySelector('#movieSelect')
+    var movieIndex = movieSelect.value;
+    var movie;
+    for (var i = 0; i < movieList.length; i++) {
+        if (movieList[i].movieId == parseInt(movieIndex)) {
+            movie = movieList[i];
+        }
+    }
+
+    var programSelect = document.querySelector('#programSelect');
+    var programIndex = programSelect.value;
+    var program;
+    for (var i = 0; i < programList.length; i++) {
+        if (programList[i].programId == parseInt(programIndex)) {
+            program = programList[i];
+        }
+    }
+
+    var result = program.addMovie(movie);
+
+    if(result == "Movie already in program"){
+        var text = document.createTextNode(result);
+        var errorDiv = document.querySelector("#errorText");
+        errorDiv.appendChild(text);
+    }
+    var li = document.querySelector("li[data-programIndex = '" + programIndex + "']");
+    var programInfo = program.getData();
+    li.textContent = programInfo;
+
+    if(result == 'More then 8h of movies or more than 4 of same genre'){
+        var text = document.createTextNode(result);
+        var errorDiv = document.querySelector("#errorText");
+        errorDiv.appendChild(text);
+    }
+
+    var li = document.querySelector("li[data-programIndex = '" + programIndex + "']");
+    var programInfo = program.getData();
+    li.textContent = programInfo;
+   
+    movieSelect.value = "-";
+    programSelect.value = "-";
+};
+
+// TODO update program data, messages
