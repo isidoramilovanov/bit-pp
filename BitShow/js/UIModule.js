a46a281e5255378c20aa5d8058174644ac089124
BIT Student <classroom@bgit.rs>
2018-03-12 09:26:09 +0100

    BitShow3

--- BitShow/js/UIModule.js
+++ BitShow/js/UIModule.js
@@ -1,71 +1,79 @@
-const UIModule = (function () {
+const UIModule = (function() {
+  function displayShows(tvShows) {
+    tvShows.forEach(function(element) {
+      var d = $("<div>");
+      var picture = $("<img>");
+      var link = $("<a>");
+      $(link).attr("class", "one-movie");
+      $(link).attr("href", `showInfoPage.html#${element.id}`);
+      $(picture).attr("src", element.image);
+      $(d).attr("class", "col-4");
+      $(picture).css("width", "100%");
+      $(d).css("text-align", "center");
+      $(link).append(picture);
+      $(link).append(element.name);
+      $(d).append(link);
+      $(".row").append(d);
+    });
+  }
 
-    function displayShows(tvShows) {
+  function displayShow(show) {
+    var d1 = $("<div>");
+    $(d1).attr("class", "row");
+    var title = $("<h1>");
+    var picture1 = $("<img>");
+    var pictureDiv = $("<div>");
+    $(pictureDiv).attr("class", "col-8");
+    $(pictureDiv).append(picture1);
+    var infoDiv = $("<div>");
+    $(infoDiv).attr("class", "col-4");
 
-        tvShows.forEach(function (element) {
+    var details = $("<p>");
+    $(title).text(show.name);
+    $(picture1).attr("src", show.image);
+    $(picture1).css("width", "100%");
+    $(details).html(show.summary);
+    $("#info").append(title);
+    $(d1).append(pictureDiv);
 
-            var d = $('<div>');
-            var picture = $('<img>');
-            var link = $('<a>');
-            $(picture).attr("src", element.image);
-            $(link).attr('href', `showInfoPage.html#${element.id}`);
-            $(d).attr('class', 'col-4');
-            $(picture).css('width', '100%');
-            $(d).css('text-align', 'center');
-            $(link).text(element.name);
-            $(d).append(picture);
-            $(d).append(link);
-            // console.log(link);
-            $('.row').append(d);
+    $("#info").append(d1);
+    var seasonTitle = $("<h3>");
+    $(infoDiv).append(seasonTitle);
+    var count = 0;
+    var uList = $("<ul>");
+    show.seasons.forEach(function(element) {
+      var list = $("<li>");
+      $(list).text(`${element.premiereDate} - ${element.endDate}`);
+      count++;
 
-        });
-    }
+      $(uList).append(list);
+      $(infoDiv).append(uList);
+    });
+    seasonTitle.text(`Seasons (${count})`);
+    var castTitle = $("<h3>");
+    castTitle.text("Cast");
+    $(infoDiv).append(castTitle);
 
-    function displayShow(show) {
+    var castList = $("<ul>");
+    show.casts.forEach(function(element) {
+      var cList = $("<li>");
+      $(cList).text(element.name);
+      $(castList).append(cList);
+      $(infoDiv).append(castList);
+    });
 
-        var d1 = $('<div>');
-        var title = $('<h1>');
-        var picture1 = $('<img>');
-        var details = $('<p>');
-        $(title).text(show.name);
-        $(picture1).attr('src', show.image);
-        $(details).html(show.summary);
-        $(d1).append(title);
-        $(d1).append(picture1);
-        $(d1).append(details);
-        $('#info').append(d1);
+    $(d1).append(infoDiv);
+    $(d1).append(details);
+  }
+  function makeSearchList(movieName, id) {
+    var optionList = $("<option>");
+    $(optionList).text(`${movieName} ${id} `);
+    $("#search-list").append(optionList);
+  }
 
-        show.seasons.forEach(function (element) {
-            var uList = $('<ul>');
-            var list = $('<li>');
-            $(list).text(`${element.premiereDate} - ${element.endDate}`);
-            $(uList).append(list);
-            $(d1).append(uList);
-
-        })
-
-        show.casts.forEach(function(element){
-            var castList = $('<ul>');
-            var cList = $('<li>');
-            $(cList).text(element.name);
-            $(castList).append(cList);
-            $(d1).append(castList);
-        })
-       
-
-    }
-    function makeSearchList(movieName,id){
-        var optionList =$('<option>');
-        $(optionList).text(movieName);
-        $('#search-list').append(optionList);
-    }
-
-    return {
-        displayShows: displayShows,
-        displayShow: displayShow,
-        makeSearchList: makeSearchList
-    }
-
-
-
+  return {
+    displayShows: displayShows,
+    displayShow: displayShow,
+    makeSearchList: makeSearchList
+  };
 })();
--- BitShow/js/mainModule.js
+++ BitShow/js/mainModule.js
@@ -1,81 +1,119 @@
-const mainModule = (function (dataModule, UIModule) {
-
-    function init() {
-        var request = $.ajax({
-            url: `http://api.tvmaze.com/shows`,
-            method: "GET"
-        });
-        request.done(function (msg) {
-            let tvShow = new dataModule.TvShows();
-            for (let i = 0; i < 50; i++) {
-
-                var show = new dataModule.Show(msg[i].id, msg[i].name, msg[i].image.medium, msg[i].summary);
-                tvShow.addShow(show);
-            }
-            // console.log(tvShow.shows);
-            UIModule.displayShows(tvShow.shows);
-
-
-        });
-
-        $('#search_box').keyup(function(event){
-            var value =this.value;
-            var request = $.ajax({
-                url: `http://api.tvmaze.com/search/shows?q=${value}`,
-                method: "GET"
-            });
-            request.done(function (msg) {
-                msg.forEach(element => {
-                    let movieName = element.show.name;
-                    let id = element.show.id;
-                    UIModule.makeSearchList(movieName, id);
-                });
-               
-            })
-            $('#search_box').change(function(event){
-               var id =this.value;
-               console.log(event);
-            //    window.location.href = `showInfoPage.html#${id}`;
-            })
-        })
+const mainModule = (function(dataModule, UIModule) {
+  function init() {
+    var request = $.ajax({
+      url: `http://api.tvmaze.com/shows`,
+      method: "GET"
+    });
+    request.done(function(msg) {
+      let tvShow = new dataModule.TvShows();
+      for (let i = 0; i < 50; i++) {
+        var show = new dataModule.Show(
+          msg[i].id,
+          msg[i].name,
+          msg[i].image.medium,
+          msg[i].summary
+        );
+        tvShow.addShow(show);
     }
+      UIModule.displayShows(tvShow.shows);
+      $("input").val("");
+    });
+
+
+    $("input").keyup(function(event) {
+      var value = this.value;
+      var request = $.ajax({
+        url: `http://api.tvmaze.com/search/shows?q=${value}`,
+        method: "GET"
+      });
+
+        $("datalist").empty();
+
+      request.done(function(msg) {
+       for (let i = 0; i<10;i++) {
+          let movieName = msg[i].show.name;
+          let id = msg[i].show.id;
+             UIModule.makeSearchList(movieName, id);  
+        };
+      });
+
+      $("input").change(function(event) {
+         
+        var idAtt = $(this).val().split(" ");
+        var id = idAtt[idAtt.length - 1];
+        window.location.href = `showInfoPage.html#${id}`;
+      });
+    });
+  }
 
 
 
-    function infoPage() {
-        var id = window.location.hash.slice(1);
-        var request = $.ajax({
-            url: `http://api.tvmaze.com/shows/${id}`,
-            method: "GET",
-            data: {
-                embed: ['seasons', 'cast']
-            }
+  function infoPage() {
+    var id = window.location.hash.slice(1);
+    var request = $.ajax({
+      url: `http://api.tvmaze.com/shows/${id}`,
+      method: "GET",
+      data: {
+        embed: ["seasons", "cast"]
+      }
+    });
+
+    request.done(function(msg) {
+      let show = new dataModule.Show(
+        msg.id,
+        msg.name,
+        msg.image.original,
+        msg.summary
+      );
+
+      for (let i = 0; i < msg._embedded.seasons.length; i++) {
+        let seasonInfo = new dataModule.Season(
+          msg._embedded.seasons[i].premiereDate,
+          msg._embedded.seasons[i].endDate
+        );
+        show.addSeason(seasonInfo);
+      }
+
+      for (let i = 0; i < msg._embedded.cast.length; i++) {
+        let castInfo = new dataModule.Cast(msg._embedded.cast[i].person.name);
+        show.addCast(castInfo);
+      }
+      UIModule.displayShow(show);
+    });
+       
+  }
+
+  function changePage(){
+     $("input").keyup(function(event) {
+       var value = this.value;
+       var request = $.ajax({
+         url: `http://api.tvmaze.com/search/shows?q=${value}`,
+         method: "GET"
+       });
+
+       $("datalist").empty();
+
+       request.done(function(msg) {
+         for (let i = 0; i < 10; i++) {
+           let movieName = msg[i].show.name;
+           let id = msg[i].show.id;
+
+           UIModule.makeSearchList(movieName, id);
+           
+         }
+          
+        
+       });
+
+        $("input").change(function(event) {
+           window.location.href = `show.html`;
         });
+     });
+  }  
 
-        request.done(function (msg) {
-            let show = new dataModule.Show(msg.id, msg.name, msg.image.original, msg.summary);
-
-            for (let i = 0; i < msg._embedded.seasons.length; i++) {
-                let seasonInfo = new dataModule.Season(msg._embedded.seasons[i].premiereDate, msg._embedded.seasons[i].endDate);
-                show.addSeason(seasonInfo);
-            }
-            
-            for (let i = 0; i < msg._embedded.cast.length; i++) {
-                let castInfo = new dataModule.Cast(msg._embedded.cast[i].person.name);
-                show.addCast(castInfo);
-            }
-            // console.log(show);
-            UIModule.displayShow(show);
-
-        })
-
-    }
-
-
-    return {
-        init: init,
-        infoPage: infoPage
-    }
-
-
+  return {
+    init: init,
+    infoPage: infoPage,
+    changePage :changePage
+  };
 })(dataModule, UIModule);
